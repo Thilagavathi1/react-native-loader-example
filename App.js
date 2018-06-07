66264a9ddd49314e3a63d92352c2c51690e54601
Thilagavathi1 <rthilagavathi305@gmail.com>
2018-06-07 08:50:00 +0600

    design added

--- App.js
+++ App.js
@@ -8,31 +8,36 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
-  View
+  View,Text,
+  Animated,
+  Easing
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
+
 export default class App extends Component<Props> {
+    constructor () {
+    super()
+    this.animatedValue = new Animated.Value(0)
+  }
+  
   render() {
+    const spin = this.animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+    easing: Easing.back(2)
+  })
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={{width:50,height:50,backgroundColor:'white',borderRadius:25,elevation: 10}}></View>
+      <View style={{width:50,height:50,backgroundColor:'yellow',borderRadius:25,elevation: 10}} >
+        <Animated.View  style={{
+          width: 50,
+          height: 50,
+          transform: [{rotate: spin}] }}>
+            <Text>Hello </Text>
+        </Animated.View>
+      </View>
       </View>
     );
   }
@@ -43,7 +48,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'blue',
   },
   welcome: {
     fontSize: 20,
